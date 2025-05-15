c1063ccc1aba40d684247c627bd81bba102d4c93
vallabh1608 <158051290+vallabh1608@users.noreply.github.com>
2025-05-15 10:45:20 +0000

    Variables

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -1,6 +1,12 @@
 const accountId = 92365
 let accountEmail = "vallabhkumar@gmail.com"
-var accountPassword = "53h3#5374"
-//accountCity = "jaipur"
+var accountPassword = "53h3#5374" //Better not to you due to Block scope / function scope 
+accountCity = "jaipur" //This assignment not preferred
+
+//accountId = 1213 
+accountEmail = "varu@ibm.com"
+accountPassword = "3te678y4r84y3hrfb"
+accountCity = "Hyderabad"
 
 console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
