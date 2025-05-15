1e7b6183123917c8848aa2e4eef1db3c2403122f
vallabh1608 <158051290+vallabh1608@users.noreply.github.com>
2025-05-15 10:58:44 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //Treats all js code as newer version
+let stname = "Vallabh" //String
+let rollno = 483 //Number
+let isPresent = true //boolean
+let state = null // null datatype\ type of this data type is object
+let count; // undefined datatype
+
+console.table([typeof stname, typeof rollno, typeof isPresent, typeof state, typeof count])
+//Output:-
+// ┌─────────┬─────────────┐
+// │ (index) │ Values      │
+// ├─────────┼─────────────┤
+// │stname   │ 'string'    │
+// │rollno   │ 'number'    │
+// │isPresent│ 'boolean'   │
+// │state    │ 'object'    │
+// │count    │ 'undefined' │
+// └─────────┴─────────────┘
