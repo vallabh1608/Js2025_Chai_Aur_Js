a2857869fc0d2b3c526b424285551244c53e8ccf
vallabh1608 <158051290+vallabh1608@users.noreply.github.com>
2025-06-07 10:07:51 +0000

    commit

--- Oops_js/oop.js
+++ Oops_js/oop.js
@@ -1,12 +1,29 @@
-const user = {
-    userName : "Vallabh",
-    id : 34,
-    signin : true,
+// const user = {
+//     userName : "Vallabh",
+//     id : 34,
+//     signin : true,
 
-    getUserDetails : function(){
-        // console.log(`UserName : ${this.userName} , id : ${this.id}, Signed In : ${this.signin}`);
-        console.log(this);
+//     getUserDetails : function(){
+//         // console.log(`UserName : ${this.userName} , id : ${this.id}, Signed In : ${this.signin}`);
+//         console.log(this);
         
+//     } 
+// };
+// console.log(user.getUserDetails());
+
+function UserDetails(userName, id, signin) {
+    this.userName = userName;
+    this.id = id;
+    this.signin = signin;
+    this.greeting = function() {
+        console.log(`Welcome ${this.userName}, your id is ${this.id} and signed in status is ${this.signin}`);
     }
-};
-console.log(user.getUserDetails());
+}
+const user1 =  new UserDetails("Vallabh", 34, true);
+const user2 =  new UserDetails("Varnika", 24, false);
+console.log(user1.greeting());
+console.log(user1 instanceof UserDetails); // true
+console.log(user2 instanceof Object); // true
+
+
+console.log(user2.greeting());
